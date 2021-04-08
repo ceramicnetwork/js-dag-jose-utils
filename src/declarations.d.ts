458bcded2a6ca275188bc881dcc1a0b6390bab9f
Joel Thorstensson <joel@oed.earth>
2021-04-08 12:38:25 +0200

    chore: bump package versions

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -16,18 +16,3 @@ declare module 'varint' {
     export let bytes: number
   }
 }
-
-declare module 'multihashes' {
-  interface DecodeResult {
-    code: number
-    digest: Uint8Array
-  }
-  export function encode(digest: Uint8Array, code: number | string, length?: number): Uint8Array
-  export function decode(mh: Uint8Array): DecodeResult
-}
-
-declare module 'uint8arrays' {
-  export function toString(b: Uint8Array, enc?: string): string
-  export function fromString(s: string, enc?: string): Uint8Array
-  export function concat(bs: Array<Uint8Array>): Uint8Array
-}
