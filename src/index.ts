dca1784f204dd4c5842cf124be5de03d6542b66f
Rod Vagg <rod@vagg.org>
2021-05-15 20:02:57 +1000

    feat: add JWS utils, docs, update deps, fix lint errors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as Block from 'multiformats/block'
 import { CID } from 'multiformats/cid'
 import { sha256 } from 'multiformats/hashes/sha2'
 import { identity } from 'multiformats/hashes/identity'
+import { base64url } from 'multiformats/bases/base64'
 import * as dagCBOR from '@ipld/dag-cbor'
 
 const ENC_BLOCK_SIZE = 24
@@ -15,8 +16,40 @@ export async function encodePayload(payload: Record<string, any>): Promise<Encod
   const block = await Block.encode({ value: payload, codec: dagCBOR, hasher: sha256 })
   return {
     cid: block.cid,
-    linkedBlock: block.bytes
+    linkedBlock: block.bytes,
   }
+}
+
+export function toJWSPayload(payload: EncodedPayload | CID): string {
+  let cid = CID.asCID(payload)
+  if (!cid) {
+    cid = CID.asCID(payload.cid)
+  }
+  if (!cid) {
+    throw new Error('Payload must be an EncodedPayload or a CID')
+  }
+  return base64url.encode(cid.bytes).slice(1)
+}
+
+// map a DagJWT to an array of JWT strings that verifyJWT() will be able to verify
+export function toJWSStrings(jose: Record<string, unknown>): Array<string> {
+  if (
+    typeof jose === 'object' &&
+    typeof jose.payload === 'string' &&
+    Array.isArray(jose.signatures)
+  ) {
+    return jose.signatures.map((signature: Record<string, unknown>) => {
+      if (
+        typeof signature !== 'object' ||
+        typeof signature.protected !== 'string' ||
+        typeof signature.signature !== 'string'
+      ) {
+        throw new Error('Object must be a DagJWS')
+      }
+      return `${signature.protected}.${jose.payload}.${signature.signature}` // eslint-disable-line
+    }, [])
+  }
+  throw new Error('Object must be a DagJWS')
 }
 
 function pad(b: Uint8Array, blockSize = ENC_BLOCK_SIZE): Uint8Array {
@@ -31,10 +64,6 @@ function pad(b: Uint8Array, blockSize = ENC_BLOCK_SIZE): Uint8Array {
   return bytes
 }
 
-function unpadCIDBytes(b: Uint8Array): CID {
-  return CID.decodeFirst(b)[0]
-}
-
 export async function encodeIdentityCID(obj: Record<string, any>): Promise<CID> {
   const block = await Block.encode({ value: obj, codec: dagCBOR, hasher: identity })
   return block.cid
@@ -47,10 +76,13 @@ export function decodeIdentityCID(cid: CID): Record<string, any> {
   return dagCBOR.decode(cid.multihash.digest)
 }
 
-export async function prepareCleartext(cleartext: Record<string, any>, blockSize?: number): Promise<Uint8Array> {
+export async function prepareCleartext(
+  cleartext: Record<string, any>,
+  blockSize?: number
+): Promise<Uint8Array> {
   return pad((await encodeIdentityCID(cleartext)).bytes, blockSize)
 }
 
 export function decodeCleartext(b: Uint8Array): Record<string, any> {
-  return decodeIdentityCID(unpadCIDBytes(b))
+  return decodeIdentityCID(CID.decodeFirst(b)[0])
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,17 @@
+/* eslint-env jest */
+
+import { CID } from 'multiformats/cid'
 import {
   encodePayload,
   prepareCleartext,
   decodeCleartext,
   encodeIdentityCID,
-  decodeIdentityCID
+  decodeIdentityCID,
+  toJWSPayload,
+  toJWSStrings
 } from '../src/index'
 
 describe('dag-jose-utils', () => {
-
   it('Properly encode payload', async () => {
     const payload1 = { abc: 123 }
     const enc1 = await encodePayload(payload1)
@@ -36,5 +40,41 @@ describe('dag-jose-utils', () => {
     const enc2 = await prepareCleartext(ct2)
     expect(enc2).toMatchSnapshot()
     expect(decodeCleartext(enc2).aLink.bytes).toEqual(ct2.aLink.bytes)
+  })
+
+  it('Creates a JWS payload from a CID', () => {
+    const msg = 'Payload must be an EncodedPayload or a CID'
+    let notCID = 'foireufhiuh'
+    expect(() => toJWSPayload(notCID)).toThrowError(msg)
+    // @ts-ignore
+    notCID = { my: 'payload' }
+    expect(() => toJWSPayload(notCID)).toThrowError(msg)
+    const cid = CID.parse('bafyreiejkvsvdq4smz44yuwhfymcuvqzavveoj2at3utujwqlllspsqr6q')
+    expect(toJWSPayload(cid))
+      .toBe('AXESIIlVZVHDkmZ5zFLHLhgqVhkFakcnQJ7pOibQWtcnyhH0')
+    expect(toJWSPayload({ cid, linkedBlock: new Uint8Array([1, 2]) }))
+      .toBe('AXESIIlVZVHDkmZ5zFLHLhgqVhkFakcnQJ7pOibQWtcnyhH0')
+  })
+
+  it('Creates JWS string namedtuples from DagJWS objects', () => {
+    const msg = 'Object must be a DagJWS'
+    expect(() => toJWSStrings('nope')).toThrowError(msg)
+    expect(() => toJWSStrings({ payload: 'nope' })).toThrowError(msg)
+    expect(() => toJWSStrings({ payload: 'nope', signatures: 'nope' })).toThrowError(msg)
+    expect(() => toJWSStrings({ payload: 'nope', signatures: ['nope'] })).toThrowError(msg)
+    expect(() => toJWSStrings({ payload: 'nope', signatures: [{ nope: 'nope' }] })).toThrowError(msg)
+    expect(toJWSStrings({
+      payload: 'AXESIIlVZVHDkmZ5zFLHLhgqVhkFakcnQJ7pOibQWtcnyhH0',
+      signatures: [{
+        signature: 'lxSptfM-Q9Y12o8IAjrTomyGZREeIYcIEaM9OO0IVOvhJOggkNyMnQOJnMnl5xMHmejLTSaTL2bnrqszDfBHVA',
+        protected: 'eyJhbGciOiJFUzI1NksifQ'
+      }, {
+        signature: 'kxSptfM-Q9Y12o8IAjrTomyGZREeIYcIEaM9OO0IVOvhJOggkNyMnQOJnMnl5xMHmejLTSaTL2bnrqszDfBHVB',
+        protected: 'dyJhbGciOiJFUzI1NksifR'
+      }]
+    })).toStrictEqual([
+      'eyJhbGciOiJFUzI1NksifQ.AXESIIlVZVHDkmZ5zFLHLhgqVhkFakcnQJ7pOibQWtcnyhH0.lxSptfM-Q9Y12o8IAjrTomyGZREeIYcIEaM9OO0IVOvhJOggkNyMnQOJnMnl5xMHmejLTSaTL2bnrqszDfBHVA',
+      'dyJhbGciOiJFUzI1NksifR.AXESIIlVZVHDkmZ5zFLHLhgqVhkFakcnQJ7pOibQWtcnyhH0.kxSptfM-Q9Y12o8IAjrTomyGZREeIYcIEaM9OO0IVOvhJOggkNyMnQOJnMnl5xMHmejLTSaTL2bnrqszDfBHVB'
+    ])
   })
 })
